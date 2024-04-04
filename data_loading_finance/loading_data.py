9de1cf66e3a16563230be6de8be92a9196dd7a5e
carlosherediapimienta <carlosherediapimienta@gmail.com>
2024-04-04 11:46:54 +0200

    Mejoras_Data_Loading_tickers

--- data_loading_finance/loading_data.py
+++ data_loading_finance/loading_data.py
@@ -1,9 +1,11 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
+import requests
+import warnings
 
 class LoadingData:
-    def __init__(self, tickers=None):
+    def __init__(self, tickers=None, exchanges=None, regions=None, exchange=False, byregion=False):
         """
         The function initializes a list of stock tickers, defaulting to the S&P 500 if no tickers are
         provided.
@@ -12,12 +14,106 @@ class LoadingData:
         with a list of stock tickers. If no tickers are provided, it defaults to the list of S&P 500 company
         tickers obtained from a Wikipedia page.
         """
+
+        if exchange and byregion:
+            raise ValueError("Only one of 'exchange' or 'byregion' can be True.")
+        elif not isinstance(exchange, bool) or not isinstance(byregion, bool):
+            raise ValueError("'exchange' and 'byregion' parameters must be boolean values.")
+        elif tickers is not None and (exchange or byregion):
+            raise ValueError("If 'tickers' is provided, 'exchange' and 'byregion' must both be False.")
+        elif tickers is None and  exchanges is None and regions is None and not exchange and not byregion:
+            warnings.warn("No data source selected.", UserWarning)
+        elif exchanges is not None and regions is not None:
+            raise ValueError("Only one of 'exchanges' or 'regions' can be provided.")
+        elif exchanges is not None and not exchange:
+            raise ValueError("If 'exchanges' is provided, 'exchange' must be True.")
+        elif regions is not None and not byregion:
+            raise ValueError("If 'regions' is provided, 'byregion' must be True.")
+
         if tickers is None:
-            ## Default to S&P 500
-            tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].tolist()
-        elif isinstance(tickers, str):
-            tickers = [tickers]
-        self.tickers = tickers
+            headers = {
+                'authority': 'api.nasdaq.com',
+                'accept': 'application/json, text/plain, */*',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+                'origin': 'https://www.nasdaq.com',
+                'sec-fetch-site': 'same-site',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': 'https://www.nasdaq.com/',
+                'accept-language': 'en-US,en;q=0.9',
+            }
+            tickers_list = []
+            if exchange is True:
+                if exchanges is not None:
+                    if isinstance(exchanges, list):
+                        exchange_list = exchanges
+                    elif isinstance(exchanges, str):
+                        exchange_list = [exchanges]
+                    else:
+                        raise ValueError("The 'exchanges' parameter must be a list or a string.")
+                else:
+                    exchange_list = ['nyse', 'nasdaq', 'amex']
+                for exchange in exchange_list:
+                    r = requests.get('https://api.nasdaq.com/api/screener/stocks', headers=headers, params=self.__params__('exchange',exchange))
+                    data = r.json()['data']
+                    df = pd.DataFrame(data['rows'], columns=data['headers'])
+                    if df.empty:
+                        print(f'No data found for the exchange {exchange}.')
+                    else:
+                        df_filtered = df[~df['symbol'].str.contains("\.|\^")]
+                        tickers_list.extend(df_filtered['symbol'].tolist())
+                self.tickers = tickers_list
+            elif byregion is True:
+                if regions is not None:
+                    if isinstance(regions, list):
+                        region_list  = regions
+                    elif isinstance(regions, str):
+                        region_list = [regions]
+                    else:
+                        raise ValueError("The 'exchanges' parameter must be a list or a string.")
+                else:
+                    region_list = ['AFRICA','EUROPE','ASIA','SOUTH AMERICA','NORTH AMERICA']
+                for region in region_list:
+                    r = requests.get('https://api.nasdaq.com/api/screener/stocks', headers=headers, params=self.__params__('region',region))
+                    data = r.json()['data']
+                    df = pd.DataFrame(data['rows'], columns=data['headers'])
+                    if df.empty:
+                        print(f'No data found for the region {region}.')
+                    else:
+                        df_filtered = df[~df['symbol'].str.contains("\.|\^")]
+                        tickers_list.extend(df_filtered['symbol'].tolist())
+                self.tickers = tickers_list
+        elif isinstance(tickers, list):
+            self.tickers = tickers
+        else:
+            self.tickers = [tickers]
+
+    def __params__(self, param_type, value):
+        """
+        Returns parameters for API request based on the type of parameter needed.
+
+        :param param_type: Type of parameter (ex: 'exchange' or 'region').
+        :param value: Value corresponding to the parameter type.
+        :return: Parameters for the API request.
+        """
+        common_params = (
+            ('letter', '0'),
+            ('download', 'true')
+        )
+        specific_param = (param_type, value)
+        params = common_params + (specific_param,)
+        return params
+    
+    def __get_crypto_tickers__():
+        url = 'https://api.coingecko.com/api/v3/coins/list'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            ticker_list = [crypto['symbol'] for crypto in data] 
+            return ticker_list
+        else:
+            print(f"Error: {response.status_code}")
+            return []
 
     def get_data(self, start_date=None, end_date=None)->pd.DataFrame:
         """
@@ -97,3 +193,8 @@ class LoadingData:
         except Exception as e:
             print(f"Could not get info for {self.tickers}. Error: {e}")
             return pd.DataFrame(columns=keys_of_interest)
+
+
+loader = LoadingData(regions='europe', byregion=True)
+data = loader.get_data()
+
