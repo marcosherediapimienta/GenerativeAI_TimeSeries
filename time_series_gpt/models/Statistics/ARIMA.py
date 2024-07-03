ec9ba6ebaf902148e8281dccc296044d6dc105ef
Marcos Heredia <marcosherediapimienta@gmail.com>
2024-07-03 17:47:35 +0200

    changes

--- time_series_gpt/models/Statistics/ARIMA.py
+++ time_series_gpt/models/Statistics/ARIMA.py
@@ -1,3 +1,116 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import scipy
+
+def lag_view(x, order):
+    """
+    For every value X_i create a row that lags k values: [X_i-1, X_i-2, ... X_i-k]
+    """
+    y = x.copy()
+    # Create features by shifting the window of `order` size by one step.
+    # This results in a 2D array [[t1, t2, t3], [t2, t3, t4], ... [t_k-2, t_k-1, t_k]]
+    x = np.array([y[-(i + order):][:order] for i in range(y.shape[0])])
+    
+    # Reverse the array as we started at the end and remove duplicates.
+    # Note that we truncate the features [order -1:] and the labels [order]
+    # This is the shifting of the features with one time step compared to the labels
+    x = np.stack(x)[::-1][order - 1: -1]
+    y = y[order:]
+
+    return x, y
+
+def ma_process(eps, theta):
+    """
+    Creates an MA(q) process with a zero mean (mean not included in implementation).
+    :param eps: (array) White noise signal.
+    :param theta: (array/ list) Parameters of the process.
+    """
+    # reverse the order of theta as Xt, Xt-1, Xt-k in an array is Xt-k, Xt-1, Xt.
+    theta = np.array([1] + list(theta))[::-1][:, None]
+    eps_q, _ = lag_view(eps, len(theta))
+    return eps_q @ theta
+
+fig = plt.figure(figsize=(18, 4 * 3))
+a = 310
+for i in range(0, 11, 5):
+    a += 1
+    theta = np.random.uniform(0, 1, size=i + 1)
+    plt.subplot(a)
+    plt.title(f'$\\theta$ = {theta.round(2)}')
+    plt.plot(ma_process(eps, theta))
+
+def pearson_correlation(x, y):
+    return np.mean((x - x.mean()) * (y - y.mean())) / (x.std() * y.std())
+
+def acf(x, lag=40):
+    """
+    Determine autocorrelation factors.
+    :param x: (array) Time series.
+    :param lag: (int) Number of lags.
+    """
+    return np.array([1] + [pearson_correlation(x[:-i], x[i:]) for i in range(1, lag)])
+
+lag = 40
+# Create an ma(1) and an ma(2) process.
+ma_1 = ma_process(eps, [1])
+ma_2 = ma_process(eps, [0.2, -0.3, 0.8])
+
+def bartletts_formula(acf_array, n):
+    """
+    Computes the Standard Error of an acf with Bartlet's formula
+    Read more at: https://en.wikipedia.org/wiki/Correlogram
+    :param acf_array: (array) Containing autocorrelation factors
+    :param n: (int) Length of original time series sequence.
+    """
+    # The first value has autocorrelation with it self. So that values is skipped
+    se = np.zeros(len(acf_array) - 1)
+    se[0] = 1 / np.sqrt(n)
+    se[1:] = np.sqrt((1 + 2 * np.cumsum(acf_array[1:-1]**2)) / n )
+    return se
+
+
+def plot_acf(x, alpha=0.05, lag=40):
+    """
+    :param x: (array)
+    :param alpha: (flt) Statistical significance for confidence interval.
+    :parm lag: (int)
+    """
+    acf_val = acf(x, lag)
+    plt.figure(figsize=(16, 4))
+    plt.vlines(np.arange(lag), 0, acf_val)
+    plt.scatter(np.arange(lag), acf_val, marker='o')
+    plt.xlabel('lag')
+    plt.ylabel('autocorrelation')
+    
+    # Determine confidence interval
+    ci = stats.norm.ppf(1 - alpha / 2.) * bartletts_formula(acf_val, len(x))
+    plt.fill_between(np.arange(1, ci.shape[0] + 1), -ci, ci, alpha=0.25)
+
+for array in [ma_1, ma_2]:
+    plot_acf(array)
+
+def ar_process(eps, phi):
+    """
+    Creates a AR process with a zero mean.
+    """
+    # Reverse the order of phi and add a 1 for current eps_t
+    phi = np.r_[1, phi][::-1] 
+    ar = eps.copy()
+    offset = len(phi)
+    for i in range(offset, ar.shape[0]):
+        ar[i - 1] = ar[i - offset: i] @ phi
+    return ar
+
+fig = plt.figure(figsize=(16, 4 * 3))
+a = 310
+for i in range(0, 11, 5):
+    a += 1
+    phi = np.random.normal(0, 0.1, size=i + 1)
+    plt.subplot(a)
+    plt.title(f'$\\phi$ = {phi.round(2)}')
+    plt.plot(ar_process(eps, phi))
+
 def least_squares(x, y):
     return np.linalg.inv((x.T @ x)) @ (x.T @ y)
 
@@ -29,7 +142,64 @@ class LinearModel:
     def fit_predict(self, x, y):
         self.fit(x, y)
         return self.predict(x)
+    
+def pacf(x, lag=40):
+    """
+    Partial autocorrelation function.
+    
+    pacf results in:
+        [1, acf_lag_1, pacf_lag_2, pacf_lag_3]
+    :param x: (array)
+    :param lag: (int)
+    """
+    y = []
+    
+    # Partial auto correlation needs intermediate terms.
+    # Therefore we start at index 3
+    for i in range(3, lag + 2):
+        backshifted = lag_view(x, i)[0]
 
+        xt = backshifted[:, 0]
+        feat = backshifted[:, 1:-1]
+        xt_hat = LinearModel(fit_intercept=False).fit_predict(feat, xt)
+
+        xt_k = backshifted[:, -1]
+        xt_k_hat = LinearModel(fit_intercept=False).fit_predict(feat, xt_k)
+
+        y.append(pearson_correlation(xt - xt_hat, xt_k - xt_k_hat))
+    return np.array([1, acf(x, 2)[1]] +  y)
+
+def plot_pacf(x, alpha=0.05, lag=40, title=None):
+    """
+    :param x: (array)
+    :param alpha: (flt) Statistical significance for confidence interval.
+    :parm lag: (int)
+    """
+    pacf_val = pacf(x, lag)
+    plt.figure(figsize=(16, 4))
+    plt.vlines(np.arange(lag + 1), 0, pacf_val)
+    plt.scatter(np.arange(lag + 1), pacf_val, marker='o')
+    plt.xlabel('lag')
+    plt.ylabel('autocorrelation')
+    
+    # Determine confidence interval
+    ci = stats.norm.ppf(1 - alpha / 2.) * bartletts_formula(pacf_val, len(x))
+    plt.fill_between(np.arange(1, ci.shape[0] + 1), -ci, ci, alpha=0.25)
+
+def difference(x, d=1):
+    if d == 0:
+        return x
+    else:
+        x = np.r_[x[0], np.diff(x)]
+        return difference(x, d - 1)
+    
+def undo_difference(x, d=1):
+    if d == 1:
+        return np.cumsum(x)
+    else:
+        x = np.cumsum(x)
+        return undo_difference(x, d - 1)
+    
 class ARIMA(LinearModel):
     def __init__(self, q, d, p):
         """
